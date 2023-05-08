be0999adaa94557b40eb1f6f690324a009aa68a0
mh <minh.huynh@arcelormittal.com>
2023-05-08 15:37:55 -0400

    install newtonsoftjson

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
     
-}).AddXmlDataContractSerializerFormatters();
+}).AddXmlDataContractSerializerFormatters().AddNewtonsoftJson();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
