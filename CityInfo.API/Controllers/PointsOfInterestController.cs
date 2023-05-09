142fb4a6231dc8369d66cf449f2737220c96d11a
vibqetowi <abutcher.usa@protonmail.com>
2023-05-09 11:21:21 -0400

    add basic logger

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -2,34 +2,74 @@ using CityInfo.API.Models.POI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
 
+#pragma warning disable CS8602
 namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Route("api/cities/{cityId}/poi")]
     public class PointsOfInterestController : ControllerBase 
     {
+        private readonly ILogger<PointsOfInterestController> logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> loggerIn)
+        {
+            this.logger = loggerIn ?? throw new ArgumentNullException(nameof(loggerIn));
+        }
+
         // GET /api/cities/{cityId}/poi
         [HttpGet]
         public ActionResult<IEnumerable<PointOfInterestDto>> GetPOIs(int cityId)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            return city == null ? NotFound() : Ok(city.POIs);
+            
+            try {
+
+                // throw new Exception("test");        
+                var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+                if (city == null)
+                {
+                    this.logger.LogInformation($"City with ID {cityId} was not found");
+                    return NotFound();
+                }
+                else
+                {
+                    this.logger.LogInformation($"Retrieved points of interest for city {city.Name}");
+                    return Ok(city.POIs);
+                }
+        
+             } catch (Exception e){
+                this.logger.LogCritical("Exception occured",e);
+                return StatusCode(500, "An internal error occurred");
+            }
         }
 
         // GET /api/cities/{cityId}/poi/{poiId}
         [HttpGet("{poiId}", Name = "GetPOI")]
         public ActionResult<PointOfInterestDto> GetPOI(int cityId, int poiId)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId); 
-            var poi = city?.POIs.FirstOrDefault(p => p.Id == poiId);
-            return poi == null ? NotFound() : Ok(poi);
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
+
+            if (targetPOI == null)
+            {
+                this.logger.LogInformation($"Point of interest with ID {poiId} was not found in city {city.Name}");
+                return NotFound();
+            }
+            else
+            {
+                this.logger.LogInformation($"Retrieved point of interest {targetPOI.Name} in city {city.Name}");
+                return Ok(targetPOI);
+            }
         }
 
         [HttpPost]
         public ActionResult<PointOfInterestDto> CreatePOI(int cityId, [FromBody] CreatePointOfInterestDto newPOI)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId); 
-            if (city == null) return NotFound();
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                this.logger.LogInformation($"City with ID {cityId} was not found");
+                return NotFound();
+            }
 
             var newPOIId = CitiesDataStore.Current.Cities.SelectMany(c => c.POIs).Max(p => p.Id) + 1;
 
@@ -41,58 +81,87 @@ namespace CityInfo.API.Controllers
             };
 
             city.POIs.Add(processedPOI);
-        
+
+            this.logger.LogInformation($"Added new point of interest {newPOI.Name} to city {city.Name}");
+
             return CreatedAtRoute("GetPOI", new { cityId = cityId, poiId = newPOIId }, processedPOI);
         }
 
         [HttpPut("{poiId}")]
         public ActionResult FullyUpdatePOI(int cityId, int poiId, UpdatePOIDto inputPOI)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId); 
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
-            if (targetPOI == null) return NotFound();
-        
+
+            if (targetPOI == null)
+            {
+                this.logger.LogInformation($"Point of interest with ID {poiId} was not found in city {city.Name}");
+                return NotFound();
+            }
+
             targetPOI.Name = inputPOI.Name;
             targetPOI.Description = inputPOI.Description;
-        
-            return Ok(targetPOI);
-        }
 
-        [HttpPatch("{poiId}")]
-        public ActionResult PartiallyUpdatePOI(int cityId, int poiId, JsonPatchDocument<UpdatePOIDto> poiPatch)
+            this.logger.LogInformation($"Updated point of interest {targetPOI.Name} in city {city.Name}");
+
+
+        return Ok(targetPOI);
+    }
+
+    [HttpPatch("{poiId}")]
+    public ActionResult PartiallyUpdatePOI(int cityId, int poiId, JsonPatchDocument<UpdatePOIDto> poiPatch)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
+
+        if (targetPOI == null)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if (city == null) return NotFound();
-            
-            var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
-            if (targetPOI == null) return NotFound();
-
-            var patchedPOI = new UpdatePOIDto()
-            {
-                Name = targetPOI.Name,
-                Description = targetPOI.Description
-            };
-
-            poiPatch.ApplyTo(patchedPOI);
-    
-            return !TryValidateModel(patchedPOI) ? BadRequest(ModelState) : Ok(targetPOI);
+            this.logger.LogInformation($"Point of interest with ID {poiId} was not found in city {city.Name}");
+            return NotFound();
         }
 
-        [HttpDelete("{poiId}")]
-        public ActionResult DeletePOI(int cityId, int poiId)
+        var patchedPOI = new UpdatePOIDto()
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if (city == null) return NotFound();
+            Name = targetPOI.Name,
+            Description = targetPOI.Description
+        };
 
-            var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
-            if (targetPOI == null) return NotFound();
+        poiPatch.ApplyTo(patchedPOI);
 
-            #pragma warning disable CS8602
-            city.POIs.Remove(targetPOI);
-            #pragma warning restore CS8602
-
-            return NoContent();
+        if (!TryValidateModel(patchedPOI))
+        {
+            this.logger.LogInformation($"Failed to update point of interest {targetPOI.Name} in city {city.Name}");
+            return BadRequest(ModelState);
         }
 
-    }   
+        targetPOI.Name = patchedPOI.Name;
+        targetPOI.Description = patchedPOI.Description;
+
+        this.logger.LogInformation($"Updated point of interest {targetPOI.Name} in city {city.Name}");
+
+        return Ok(targetPOI);
+    }
+
+    [HttpDelete("{poiId}")]
+    public ActionResult DeletePOI(int cityId, int poiId)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        var targetPOI = city?.POIs.FirstOrDefault(p => p.Id == poiId);
+
+        if (targetPOI == null)
+        {
+            this.logger.LogInformation($"Point of interest with ID {poiId} was not found in city {city.Name}");
+            return NotFound();
+        }
+
+        city.POIs.Remove(targetPOI);
+
+        this.logger.LogInformation($"Deleted point of interest {targetPOI.Name} in city {city.Name}");
+
+        return NoContent();
+    }
+
+}   
+
 }
+#pragma warning restore CS8602
