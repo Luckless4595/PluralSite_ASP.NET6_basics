4a8710c9ad5a913b46adc280cb384fe4199b9114
vibqetowi <abutcher.usa@protonmail.com>
2023-05-10 12:52:06 -0400

    added a db migration

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -12,14 +12,16 @@ namespace CityInfo.API.Entities{
         [Required]
         [MaxLength(50)]
         public string Name{ get; set; }
-        // public string? Description{get;set;}
+        [MaxLength(200)]
+        public string? Description{get;set;}
 
         [ForeignKey("CityId")]
         public City? City {get; set;}
         public int CityId {get; set;}
 
-        public PointOfInterest(string name){
+        public PointOfInterest(string name, string description){
             this.Name = name;
+            this.Description = description;
         }
     }
 }
